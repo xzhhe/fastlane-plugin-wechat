95c268d56f917991ed711e197c031d0819b8d322
xiongzenghui <zxcvb1234001@163.com>
2020-03-02 00:16:45 +0800

    rake

--- spec/wechat_action_spec.rb
+++ spec/wechat_action_spec.rb
@@ -2,8 +2,8 @@ describe Fastlane::Actions::WechatAction do
   describe '#run' do
     it 'request_access_token' do
       puts Fastlane::Actions::WechatAction.request_access_token(
-        'http://smsc.in.zhihu.com/token', 
-        '10024', 
+        'http://smsc.in.xxx.com/token',
+        '10024',
         'dccae6ea9369a64d5131cfcf0e75a045'
       )
     end
@@ -44,25 +44,25 @@ describe Fastlane::Actions::WechatAction do
 
     it 'send_with_token -  text' do
       Fastlane::Actions::WechatAction.send_with_token(
-        access_token_url: 'http://smsc.in.zhihu.com/token',
+        access_token_url: 'http://smsc.in.xxx.com/token',
         agentid: '10024',
         secret: 'dccae6ea9369a64d5131cfcf0e75a045',
         recievers: ['xiongzenghui'],
         msgtype: 'text',
         text: "xxxxxxxxxx",
-        send_message_url: "http://smsc.in.zhihu.com/message/send?userid=xiongzenghui"
+        send_message_url: "http://smsc.in.xxx.com/message/send?userid=xiongzenghui"
       )
     end
 
     it 'send_with_token -  markdown' do
       Fastlane::Actions::WechatAction.send_with_token(
-        access_token_url: 'http://smsc.in.zhihu.com/token',
+        access_token_url: 'http://smsc.in.xxx.com/token',
         agentid: '10024',
         secret: 'dccae6ea9369a64d5131cfcf0e75a045',
         recievers: ['xiongzenghui'],
         msgtype: 'markdown',
         text: "实时新增用户反馈<font color=\"warning\">132例</font>，请相关同事注意.\n >类型:<font color=\"comment\">用户反馈</font>\n >普通用户反馈:<font color=\"comment\">117例</font>\n >VIP用户反馈:<font color=\"comment\">15例</font>",
-        send_message_url: "http://smsc.in.zhihu.com/message/send?userid=xiongzenghui"
+        send_message_url: "http://smsc.in.xxx.com/message/send?userid=xiongzenghui"
       )
     end
 
@@ -84,25 +84,25 @@ describe Fastlane::Actions::WechatAction do
 
     it 'run - send_with_token - text' do
       Fastlane::Actions::WechatAction.run(
-        access_token_url: 'http://smsc.in.zhihu.com/token',
+        access_token_url: 'http://smsc.in.xxx.com/token',
         agentid: '10024',
         secret: 'dccae6ea9369a64d5131cfcf0e75a045',
         recievers: ['xiongzenghui'],
         msgtype: 'text',
         text: "xxxxxxxxxx",
-        send_message_url: "http://smsc.in.zhihu.com/message/send?userid=xiongzenghui"
+        send_message_url: "http://smsc.in.xxx.com/message/send?userid=xiongzenghui"
       )
     end
 
     it 'run - send_with_token - markdown' do
       Fastlane::Actions::WechatAction.run(
-        access_token_url: 'http://smsc.in.zhihu.com/token',
+        access_token_url: 'http://smsc.in.xxx.com/token',
         agentid: '10024',
         secret: 'dccae6ea9369a64d5131cfcf0e75a045',
         recievers: ['xiongzenghui'],
         msgtype: 'markdown',
         text: "实时新增用户反馈<font color=\"warning\">132例</font>，请相关同事注意.\n >类型:<font color=\"comment\">用户反馈</font>\n >普通用户反馈:<font color=\"comment\">117例</font>\n >VIP用户反馈:<font color=\"comment\">15例</font>",
-        send_message_url: "http://smsc.in.zhihu.com/message/send?userid=xiongzenghui"
+        send_message_url: "http://smsc.in.xxx.com/message/send?userid=xiongzenghui"
       )
     end
 
